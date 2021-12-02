c3a0ee3010cca9f428aea14db37755fb5f03e100
tharinda <tharindahp@gmail.com>
2021-12-02 23:46:02 +0530

    add validation middleware

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
 
-router.post("/signup", todoController.addTodo);
+router.post(
+  "/signup",
+  [
+    body("email").isEmail().withMessage("Invalid Email").normalizeEmail(),
+    body("password")
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage("Password must have at least 5 characters"),
+  ],
+  authController.signup
+);
+
+router.post("/login", authController.login);
 
 module.exports = router;
